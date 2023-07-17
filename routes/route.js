744504eda9812f5dcabe90981e0c8d832e8a435a
Atanu Paul <atanupaul2411@gmail.com>
2023-07-17 21:51:36 +0530

    authorisation enabled

--- routes/route.js
+++ routes/route.js
@@ -6,18 +6,18 @@ const router = express.Router();
 const { forwardAuthenticated, ensureAuthenticated } = require('../config/auth');
 
 // routes for all pages
-router.get('/', formControllers.homePage);
-router.get('/login', formControllers.loginPage);
-router.get('/register', formControllers.registerPage);
-router.get('/dashboard', formControllers.dashboard);
-router.get('/edit', formControllers.editPage);
+router.get('/', forwardAuthenticated, formControllers.homePage);
+router.get('/login', forwardAuthenticated, formControllers.loginPage);
+router.get('/register', forwardAuthenticated, formControllers.registerPage);
+router.get('/dashboard', ensureAuthenticated, formControllers.dashboard);
+router.get('/edit', ensureAuthenticated, formControllers.editPage);
 
 // create a new account
 router.post('/register', formControllers.registerAccount);
 
 // Login to Dashboard
 router.post("/login", passport.authenticate("local", { failureRedirect: "/login", failureFlash: true, }), (req, res) => {
-  // req.flash("success_msg", "You have successfully logged in!");
+  req.flash("success_msg", "You have successfully logged in!");
   res.redirect("/dashboard");
 });
 
